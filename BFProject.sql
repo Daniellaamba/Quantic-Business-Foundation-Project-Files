06ac4bd6fa36b4b3dcf58fbd0852d11d9895517f
Biometrics-IITA <43005268+Daniellaamba@users.noreply.github.com>
2025-07-27 22:22:50 +0100

    update queries

--- BFProject.sql
+++ BFProject.sql
@@ -37,7 +37,7 @@ COUNT(*) AS total_loans,
 ROUND(AVG(current_loan_amount), 2) AS avg_loan_amount
 FROM loan.Loans
 GROUP BY loan_type
-ORDER BY avg_loan_amount DESC;
+ORDER BY total_loans DESC;
 
 --QUERY 2: CREDIT SCORE ANALYSIS
 SELECT c.customer_id, l.credit_score,
@@ -47,8 +47,7 @@ JOIN loan.loans l
 ON c.customer_id = l.customer_id
 WHERE l.credit_score IS NOT NULL
 GROUP BY c.customer_id, l.credit_score
-ORDER BY l.credit_score DESC
-LIMIT 5;
+ORDER BY l.credit_score DESC;
 
 --QUERY 3: MONTHLY REVENUE FROM PERFORMING LOANS
 
@@ -58,12 +57,11 @@ WHERE loan_status = 'Fully Paid';
 
 
 --QUERY 4: Months passed since a borrower last missed a scheduled loan payment
-
-SELECT l.credit_score,l.current_loan_amount
-FROM loan.loans l
-JOIN loan.customers c ON l.customer_id = c.customer_id
-WHERE l.credit_score IS NOT NULL
-ORDER BY l.current_loan_amount
+select c.customer_id, l.months_since_last_delinquent
+from loan.customers c
+join loan.loans l ON c.customer_id = l.customer_id
+where l.months_since_last_delinquent IS NOT NULL
+order by l.months_since_last_delinquent DESC
 LIMIT 10;
 
 -- QUERY 5: Total number of Open Accounts per customer:
@@ -74,28 +72,15 @@ where l.number_open_accounts IS NOT NULL
 order by l.number_open_accounts DESC
 LIMIT 10;
 
---QUERY 6: Number pf Credit Problems
-select c.customer_id, l.months_since_last_delinquent
+--QUERY 6: Number of Credit Problems
+select c.customer_id, l.number_credit_problems
 from loan.customers c
 join loan.loans l ON c.customer_id = l.customer_id
-where l.months_since_last_delinquent IS NOT NULL
-order by l.months_since_last_delinquent DESC
-LIMIT 10;
+where l.number_credit_problems IS NOT NULL
+order by l.number_credit_problems DESC;
+
 
 -- 7. DEFAULT RATE BY LOAN TYPE
-
-SELECT 
-  c.years_in_current_job,
-  COUNT(*) AS total_loans,
-  SUM(CASE WHEN l.loan_status = 'Charged Off' THEN 1 ELSE 0 END) AS defaults,
-  ROUND(100.0 * SUM(CASE WHEN l.loan_status = 'Charged Off' THEN 1 ELSE 0 END) / COUNT(*), 2) AS default_rate_percent
-FROM Loans l
-JOIN Customers c ON l.customer_id = c.customer_id
-GROUP BY c.years_in_current_job
-ORDER BY default_rate_percent DESC;
-
-
---8 OVERALL DEFAULT RATE
 SELECT 
   term,
   COUNT(*) AS total_loans,
@@ -105,7 +90,13 @@ FROM loan.Loans
 GROUP BY term
 ORDER BY default_rate_percent DESC;
 
---QUERY 9: Loan status by Job tenure
+--8 OVERALL DEFAULT RATE
+SELECT 
+	ROUND(SUM(CASE WHEN loan_status = 'Charged Off' THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) 
+AS default_rate_percentage
+FROM loan.Loans;
+
+--QUERY 9: Home status by Job tenure
 SELECT 
     c.years_in_current_job,
     l.loan_status,
@@ -124,13 +115,12 @@ GROUP BY home_ownership, term
 ORDER BY count,
 count DESC;
 
--- QUERY 11. Average Loan Amount y Home Ownership
-SELECT c.customer_id, l.credit_score,
-       COUNT(*) AS count
-FROM loan.customers c
-JOIN loan.loans l
-ON c.customer_id = l.customer_id
-WHERE l.credit_score IS NOT NULL
-GROUP BY c.customer_id, l.credit_score
-ORDER BY l.credit_score DESC;
+--QUERY 11: Average Loan Amount by Home Ownership
 
+SELECT 
+Loan_type,
+COUNT(*) AS total_loans,
+ROUND(AVG(current_loan_amount), 2) AS avg_loan_amount
+FROM loan.Loans
+GROUP BY loan_type
+ORDER BY loan_type DESC;
